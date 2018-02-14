2e2ce44131679f11cef374a2cfb8a2b8b89a857f
Subba Reddy <asubbareddy@gmail.com>
2018-02-13 18:53:31 -0800

    snp500 extract

--- zacksrank/rank.py
+++ zacksrank/rank.py
@@ -2,61 +2,65 @@
 # https://stackoverflow.com/questions/22003302/beautiful-soup-just-get-the-value-inside-the-tag
 
 import requests
-import csv
 from bs4 import BeautifulSoup
 import pandas as pd
 import datetime
-#from sp500_list import sp500_symbols
 
-# import module sys to get the type of exception
-import sys
+import sys # import module sys to get the type of exception
 
-# new functkon 
-def get_symbols():
-
+# *****  extract_SnP500_symbols() function *********
+#
+# **************************************************
+def extract_SnP500_symbols():
+    snp500_list = []
+    try:  
+        url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies' 
+        page = requests.get(url)
+        #print(page.status_code)
+        soup = BeautifulSoup(page.text, 'html.parser')
     
+        # extract first table which is symbol list
+        table = soup.find('table', class_='wikitable')
+        trs = table.find_all('tr')
+        #print(len(trs))
+        
+        
+        for tr in trs:
+            try:  
+                tds = tr.find_all('td')
+                symbol = str(tds[0].get_text())
+                name = str(tds[1].get_text())
+                #print(symbol, name)
+                snp500_list.append([symbol, name])
+                
+            except:
+                # first <tr> is table header, it won't have <td> so handle exception here
+                print('While extracting <tr> Eror Occured!',sys.exc_info()[0])   
     
-    sp500_symbols = [ 'ABT', 'ABBV', 'ACN', 'ACE', 'ADBE', 'ADT', 'AAP', 'AES', 'AET', 'AFL', 'AMG', 'A', 'GAS',  
-               'APD', 'ARG', 'AKAM', 'AA', 'AGN', 'ALXN', 'ALLE', 'ADS', 'ALL', 'ALTR', 'MO', 'AMZN', 'AEE',  
-               'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'APC', 'ADI', 'AON',  
-               'APA', 'AIV', 'AMAT', 'ADM', 'AIZ', 'T', 'ADSK', 'ADP', 'AN', 'AZO', 'AVGO', 'AVB', 'AVY',  
-               'BHI', 'BLL', 'BAC', 'BK', 'BCR', 'BXLT', 'BAX', 'BBT', 'BDX', 'BBBY', 'BRK-B', 'BBY', 'BLX',  
-               'HRB', 'BA', 'BWA', 'BXP', 'BSK', 'BMY', 'BRCM', 'BF-B', 'CHRW', 'CA', 'CVC', 'COG', 'CAM', 'CPB',  
-               'COF', 'CAH', 'HSIC', 'KMX', 'CCL', 'CAT', 'CBG', 'CBS', 'CELG', 'CNP', 'CTL', 'CERN', 'CF', 'SCHW', 'CHK',  
-               'CVX', 'CMG', 'CB', 'CI', 'XEC', 'CINF', 'CTAS', 'CSCO', 'C', 'CTXS', 'CLX', 'CME', 'CMS', 'COH', 'KO', 'CCE',  
-               'CTSH', 'CL', 'CMCSA', 'CMA', 'CSC', 'CAG', 'COP', 'CNX', 'ED', 'STZ', 'GLW', 'COST', 'CCI', 'CSX', 'CMI', 'CVS',  
-               'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DLPH', 'DAL', 'XRAY', 'DVN', 'DO', 'DTV', 'DFS', 'DISCA', 'DISCK', 'DG', 'DLTR',  
-               'D', 'DOV', 'DOW', 'DPS', 'DTE', 'DD', 'DUK', 'DNB', 'ETFC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA',  
-               'EMC', 'EMR', 'ENDP', 'ESV', 'ETR', 'EOG', 'EQT', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ES', 'EXC', 'EXPE',  
-               'EXPD', 'ESRX', 'XOM', 'FFIV', 'FB', 'FAST', 'FDX', 'FIS', 'FITB', 'FSLR', 'FE', 'FSIV', 'FLIR', 'FLS',  
-               'FLR', 'FMC', 'FTI', 'F', 'FOSL', 'BEN', 'FCX', 'FTR', 'GME', 'GPS', 'GRMN', 'GD', 'GE', 'GGP', 'GIS',  
-               'GM', 'GPC', 'GNW', 'GILD', 'GS', 'GT', 'GOOGL', 'GOOG', 'GWW', 'HAL', 'HBI', 'HOG', 'HAR', 'HRS', 'HIG',  
-               'HAS', 'HCA', 'HCP', 'HCN', 'HP', 'HES', 'HPQ', 'HD', 'HON', 'HRL', 'HSP', 'HST', 'HCBK', 'HUM', 'HBAN',  
-               'ITW', 'IR', 'INTC', 'ICE', 'IBM', 'IP', 'IPG', 'IFF', 'INTU', 'ISRG', 'IVZ', 'IRM', 'JEC', 'JBHT', 'JNJ',  
-               'JCI', 'JOY', 'JPM', 'JNPR', 'KSU', 'K', 'KEY', 'GMCR', 'KMB', 'KIM', 'KMI', 'KLAC', 'KSS', 'KRFT', 'KR', 'LB',  
-               'LLL', 'LH', 'LRCX', 'LM', 'LEG', 'LEN', 'LVLT', 'LUK', 'LLY', 'LNC', 'LLTC', 'LMT', 'L', 'LOW', 'LYB', 'MTB',  
-               'MAC', 'M', 'MNK', 'MRO', 'MPC', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MAT', 'MKC', 'MCD', 'MHFI', 'MCK', 'MJN',  
-               'MMV', 'MDT', 'MRK', 'MET', 'KORS', 'MCHP', 'MU', 'MSFT', 'MHK', 'TAP', 'MDLZ', 'MON', 'MNST', 'MCO', 'MS', 'MOS',  
-               'MSI', 'MUR', 'MYL', 'NDAQ', 'NOV', 'NAVI', 'NTAP', 'NFLX', 'NWL', 'NFX', 'NEM', 'NWSA', 'NEE', 'NLSN', 'NKE', 'NI',  
-               'NE', 'NBL', 'JWN', 'NSC', 'NTRS', 'NOC', 'NRG', 'NUE', 'NVDA', 'ORLY', 'OXY', 'OMC', 'OKE', 'ORCL', 'OI', 'PCAR',  
-               'PLL', 'PH', 'PDCO', 'PAYX', 'PNR', 'PBCT', 'POM', 'PEP', 'PKI', 'PRGO', 'PFE', 'PCG', 'PM', 'PSX', 'PNW', 'PXD',  
-               'PBI', 'PCL', 'PNC', 'RL', 'PPG', 'PPL', 'PX', 'PCP', 'PCLN', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PEG', 'PSA', 'PHM',  
-               'PVH', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RRC', 'RTN', 'O', 'RHT', 'REGN', 'RF', 'RSG', 'RAI', 'RHI', 'ROK', 'COL', 'ROP',  
-               'ROST', 'RLC', 'R', 'CRM', 'SNDK', 'SCG', 'SLB', 'SNI', 'STX', 'SEE', 'SRE', 'SHW', 'SIAL', 'SPG', 'SWKS', 'SLG', 'SJM',  
-               'SNA', 'SO', 'LUV', 'SWN', 'SE', 'STJ', 'SWK', 'SPLS', 'SBUX', 'HOT', 'STT', 'SRCL', 'SYK', 'STI', 'SYMC', 'SYY',  
-               'TROW', 'TGT', 'TEL', 'TE', 'TGNA', 'THC', 'TDC', 'TSO', 'TXN', 'TXT', 'HSY', 'TRV', 'TMO', 'TIF', 'TWX', 'TWC', 'TJK', 'TMK', 'TSS',  
-               'TSCO', 'RIG', 'TRIP', 'FOXA', 'TSN', 'TYC', 'UA', 'UNP', 'UNH', 'UPS', 'URI', 'UTX', 'UHS', 'UNM', 'URBN', 'VFC', 'VLO', 'VAR', 'VTR',  
-               'VRSN', 'VZ', 'VRTX', 'VIAB', 'V', 'VNO', 'VMC', 'WMT', 'WBA', 'DIS', 'WM', 'WAT', 'ANTM', 'WFC', 'WDC', 'WU', 'WY', 'WHR', 'WFM', 'WMB',  
-               'WEC', 'WYN', 'WYNN', 'XEL', 'XRX', 'XLNX', 'XL', 'XYL', 'YHOO', 'YUM', 'ZBH', 'ZION', 'ZTS' ] 
+    except:
+        print('While extracting S&P500 Symols Eror Occured!',sys.exc_info()[0])
+  
+    return snp500_list
+
+
+# *****  get_SnP500_symbols() function *********
+#
+# **********************************************
+def get_SnP500_symbols():
+    # check if pickel file exists, if so read pickel file and return symbols
+    # else call extract() function , write pickel then return symbol list
     
-    sp500_symbols = [ 'ABT', 'ABBV', 'VRTX', 'VIABx','EBAY']
-    return sp500_symbols
+    #df.to_pickle("sp500-zacksRanks")
 
-# main program 
-#f = csv.writer(open('zackrank.csv', 'w'))
-#f.writerow(['Name', 'Link'])
-
+    #you can load it back using:
+    #df = pd.read_pickle(file_name)
+    
+    symbol_list = extract_SnP500_symbols()
+    return symbol_list
+    
 # *****  extract_rank_info() function *********
+#
+# **********************************************
 def extract_rank_info(symbol_):
     row_ = []
     try:  
@@ -89,18 +93,24 @@ def extract_rank_info(symbol_):
   
     return row_
 
-# *****  main() program *********
+
+# *****  main() program *********************
+#
+# *******************************************
 def main():
-    stocks = get_symbols()
+    #stocks = get_symbols()
+    symbol_list = get_SnP500_symbols()
+    print(len(symbol_list))
     stock_ranks = []
     
-    for symbol in stocks:  
-        row = extract_rank_info(symbol)
+    for symbol_name in symbol_list:  
+        row = extract_rank_info(symbol_name[0])
         if len(row) > 0:
             stock_ranks.append(row)
         
     print ( stock_ranks)
 
+    # create Dataframe from list and save dataframe as pickle file
     df = pd.DataFrame(stock_ranks,columns=['Symbol','Rank', 'Value', 'Growth', 'Momentum', 'VGM_composite'])
     df['Date'] = datetime.datetime.now().strftime("%Y-%m-%d")
     print (df)
@@ -112,4 +122,5 @@ def main():
 
 
 # *****  callcmain() program *********
+#
 main()
